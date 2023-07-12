76180d1ee6b5fdcd3aadba87415634aca1083a56
MishaRoyzman <yunbol@yandex.ru>
2023-07-12 13:36:26 +0300

    newYear

--- src/tale.js
+++ src/tale.js
@@ -1,13 +1,10 @@
-function kolobok(character) {
-    switch (character) {
-      case 'дедушка':
-        return 'Я от дедушки ушёл'
-      case 'заяц':
-        return 'Я от зайца ушёл'
-      case 'лиса':
-        return 'Меня съели'
+function newYear(characterName) {
+    if (characterName === 'Дед Мороз') {
+      return `${characterName}! ${characterName}! ${characterName}!`
+    } else if (characterName === 'Снегурочка') {
+      return `${characterName}! ${characterName}! ${characterName}!`
     }
 }
-console.log(kolobok('дедушка'))
-console.log(kolobok('заяц'))
-console.log(kolobok('лиса'))
+characterName = 'Дед Мороз'
+message = newYear(characterName)
+console.log(message)
